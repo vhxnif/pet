cbdf0b5228b1fe26a9a440b3803edcf231a5970a
xiaochen <chen517000@live.com>
2024-06-03 21:23:23 +0800

    opt: opt improve writing

--- src/main/kotlin/com/vhxnif/pet/service/ImproveWriting.kt
+++ src/main/kotlin/com/vhxnif/pet/service/ImproveWriting.kt
@@ -2,8 +2,8 @@ package com.vhxnif.pet.service
 
 import org.springframework.ai.chat.StreamingChatClient
 import org.springframework.ai.chat.messages.SystemMessage
-import org.springframework.ai.chat.messages.UserMessage
 import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.chat.prompt.PromptTemplate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
@@ -18,14 +18,16 @@ import reactor.core.publisher.Flux
 class ImproveWriting (
     @Value("classpath:/prompts/improveWriting/system.st")
     private val systemPrompt: Resource,
+    @Value("classpath:/prompts/improveWriting/user.st")
+    private val userPrompt: Resource,
     private val chatClient: StreamingChatClient,
 ) {
 
-    fun improve(input: String) : Flux<String> {
+    fun improve(text: String) : Flux<String> {
         return chatClient.stream(Prompt(
             listOf(
                 SystemMessage(systemPrompt),
-                UserMessage(input),
+                PromptTemplate(userPrompt).createMessage(mutableMapOf<String, Any>("text" to text))
             )
         )).map { it.result.output.content }
     }
