9441a960017c884f056037e9d0fbf384573cbb40
xiaochen <chen517000@live.com>
2024-06-10 10:20:27 +0800

    feature: add the translated output file

--- src/main/kotlin/com/vhxnif/pet/command/TranslateCommand.kt
+++ src/main/kotlin/com/vhxnif/pet/command/TranslateCommand.kt
@@ -4,9 +4,11 @@ import com.vhxnif.pet.config.annotation.Sword
 import com.vhxnif.pet.service.Translate
 import com.vhxnif.pet.transformer.TranslateTextSplitter
 import com.vhxnif.pet.util.print
+import com.vhxnif.pet.util.toFile
 import org.springframework.ai.reader.tika.TikaDocumentReader
 import org.springframework.core.io.FileSystemResource
 import picocli.CommandLine.*
+import reactor.core.publisher.Flux
 
 /**
  *
@@ -15,10 +17,7 @@ import picocli.CommandLine.*
  */
 @Sword
 @Command(
-    name = "translate",
-    aliases = ["ts"],
-    description = ["AI Translation Tool"],
-    version = ["0.1.0"]
+    name = "translate", aliases = ["ts"], description = ["AI Translation Tool"], version = ["0.1.0"]
 )
 class TranslateCommand(
     private val translate: Translate
@@ -31,7 +30,10 @@ class TranslateCommand(
     lateinit var lang: String
 
     @Option(names = ["-s", "--without-source"], description = ["Printed without source text."])
-    var printIgnoreSourceText: Boolean = false
+    var ignoreSourceText: Boolean = false
+
+    @Option(names = ["-o", "--output"], description = ["Output file path."])
+    var outputPath: String? = null
 
     class Exclusive {
         @Option(names = ["-f", "--file"], description = ["The file needs to be translated"])
@@ -42,20 +44,40 @@ class TranslateCommand(
     }
 
     override fun run() {
-        when {
-            exclusive.file != null -> TranslateTextSplitter()
-                .apply(TikaDocumentReader(FileSystemResource(exclusive.file!!)).get())
-                .map { doc -> doc.content }
-                .filter { c -> c.isNotBlank() }
-            else -> listOf(exclusive.text!!)
-        }.asSequence().map {
-            translate.translate(lang, it).doFirst {
-                if(!printIgnoreSourceText) {
-                    println(it)
+        output {
+            when {
+                exclusive.file != null -> fileContent(exclusive.file!!)
+                else -> listOf(exclusive.text!!)
+            }.asSequence().flatMap {
+                    with(translate.translate(lang, it)) {
+                        listOf(sourceTextProcess(it), this)
+                    }
                 }
-            }
-        }.print()
+        }
     }
+
+    private fun sourceTextProcess(sourceText: String): Flux<String> {
+        return if (ignoreSourceText) {
+            Flux.empty()
+        } else {
+            Flux.just(sourceText)
+        }
+    }
+
+    private fun fileContent(path: String): List<String> {
+        return TranslateTextSplitter().apply(TikaDocumentReader(FileSystemResource(path)).get())
+            .map { doc -> doc.content }.filter { c -> c.isNotBlank() }
+    }
+
+    private fun output(res: () -> Sequence<Flux<String>>) {
+        val translateRes = res()
+        if (outputPath != null) {
+            translateRes.toFile(outputPath!!)
+        } else {
+            translateRes.print()
+        }
+    }
+
 
 }
 
--- src/main/kotlin/com/vhxnif/pet/util/flux-output.kt
+++ src/main/kotlin/com/vhxnif/pet/util/flux-output.kt
@@ -1,6 +1,9 @@
 package com.vhxnif.pet.util
 
 import reactor.core.publisher.Flux
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /**
  *
@@ -19,3 +22,11 @@ fun Flux<String>.println() {
 fun Sequence<Flux<String>>.print() {
     this.reduce { acc, flux -> acc.concatWith(flux.doFirst { println() })  }.doOnNext { print(it) }.blockLast()
 }
+
+fun Sequence<Flux<String>>.toFile(path: String) {
+    Files.newBufferedWriter(Paths.get(path), StandardCharsets.UTF_8).use { writer ->
+        this.reduce { acc, flux -> acc.concatWith(flux.doFirst { writer.newLine() }) }.doOnNext{ writer.write(it) }.blockLast()
+    }
+}
+
+
